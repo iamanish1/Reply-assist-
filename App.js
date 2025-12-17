564ce9ef13cd5740eb4b7b12a50cab553aa59c57
iamanish1 <animetra14@gmail.com>
2025-12-17 20:21:05 +0530

    Revert "basic setup of app"
    This reverts commit e6aee2051998d4c8499a5b213406971f6e2c9e62.

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { ReplyFlowProvider } from './src/state/replyFlowStore';
-import { AppNavigator } from './src/navigation/AppNavigator';
-import { ThemeProvider, useTheme } from './src/theme/ThemeProvider';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <ThemedApp />
-    </ThemeProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-function ThemedApp() {
-  const { resolvedMode } = useTheme();
-  return (
-    <ReplyFlowProvider>
-      <StatusBar style={resolvedMode === 'dark' ? 'light' : 'dark'} />
-      <AppNavigator />
-    </ReplyFlowProvider>
-  );
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/theme/radii.js
+++
@@ -1,9 +0,0 @@
-export const radii = {
-  sm: 10,
-  md: 14,
-  lg: 18,
-  xl: 24,
-  pill: 999,
-};
-
-
